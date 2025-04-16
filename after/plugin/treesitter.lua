6506449ddad44fb9447a0a8e358c95c3c191d8ee
Vladislav Kudrin <vladislavkudrin@gmail.com>
2025-04-16 19:44:35 +0200

    Update config, lsp

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.configs").setup ({
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = { "javascript", "typescript",  "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
@@ -8,7 +8,9 @@ require'nvim-treesitter.configs'.setup {
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
-
+  indent = {
+    enable = true,
+  },
   highlight = {
     enable = true,
 
@@ -19,4 +21,4 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-}
+})
--- lua/vlad/plugins/lsp.lua
+++ lua/vlad/plugins/lsp.lua
@@ -1,19 +1,126 @@
-return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		{
-			"folke/lazydev.nvim",
-			ft = "lua", -- only load on lua files
-			opts = {
-				library = {
-					-- See the configuration section for more details
-					-- Load luvit types when the `vim.uv` word is found
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-				},
-			},
-		},
-	},
-	config = function() 
-		require'lspconfig'.lua_ls.setup{}
-	end,
+local root_files = {
+    '.luarc.json',
+    '.luarc.jsonc',
+    '.luacheckrc',
+    '.git',
+    "aiken.toml"
 }
+
+
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    -- See the configuration section for more details
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
+        "stevearc/conform.nvim",
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "j-hui/fidget.nvim",
+    },
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                javascript = { "prettierd" },
+                typescript = { "prettierd" },
+                javascriptreact = { "prettierd" },
+                typescriptreact = { "prettierd" },
+            }
+        })
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
+            require("fidget").setup({})
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                automatic_installation = true,
+                ensure_installed = {
+                    "lua_ls",
+                    "pyright",
+                    'ts_ls',
+                    'eslint',
+                    'html',
+                    'cssls'
+                },
+                handlers = {
+                    function(server_name) -- default handler (optional)
+                        require("lspconfig")[server_name].setup {
+                            capabilities = capabilities
+                        }
+                    end,
+                    ["lua_ls"] = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.lua_ls.setup {
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    format = {
+                                        enable = true,
+                                        -- Put format options here
+                                        -- NOTE: the value should be STRING!!
+                                        defaultConfig = {
+                                            indent_style = "space",
+                                            indent_size = "2",
+                                        }
+                                    },
+                                }
+                            }
+                        }
+                    end,
+                }
+            })
+
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ["<C-q>"] = cmp.mapping.complete(),
+                }),
+                sources = cmp.config.sources({
+                    { name = "copilot", group_index = 2 },
+                    { name = 'nvim_lsp' },
+                }, {
+                    { name = 'buffer' },
+                })
+            })
+            vim.diagnostic.config({
+                virtual_text = {
+                    prefix = "●", -- or "" or "●"
+                    spacing = 4,
+                },
+                signs = true,
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
+                float = {
+                    focusable = false,
+                    style = "minimal",
+                    border = "rounded",
+                    ---@diagnostic disable-next-line: assign-type-mismatch
+                    source = "always",
+                    header = "",
+                    prefix = "",
+                },
+            })        end,
+        }
